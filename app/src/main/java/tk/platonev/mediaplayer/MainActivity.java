1a10ed0ecdae586c89d4f6dfb24ece53988caebe
platonev <platonewang@gmail.com>
2017-06-26 15:12:13 +0800

    Alow set APP as the default video player

--- app/src/main/java/tk/platonev/mediaplayer/MainActivity.java
+++ app/src/main/java/tk/platonev/mediaplayer/MainActivity.java
@@ -1,9 +1,11 @@
 package tk.platonev.mediaplayer;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 
 public class MainActivity extends Activity {
 
@@ -16,12 +18,21 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mRecyclerView = (RecyclerView)findViewById(R.id.recyclerView);
+        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerView);
 
         mLayoutManager = new LinearLayoutManager(this);
         mRecyclerView.setLayoutManager(mLayoutManager);
 
         mAdapter = new MyAdapter(this);
         mRecyclerView.setAdapter(mAdapter);
+
+        Intent intent = getIntent();
+        if (intent.getAction().equals(Intent.ACTION_VIEW)) {
+            Intent in = new Intent(this, PlayerActivity.class);
+            Bundle bundle = new Bundle();
+            bundle.putString("file", intent.getData().getPath());
+            in.putExtras(bundle);
+            this.startActivity(in);
+        }
     }
 }
